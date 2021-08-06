11d435f6db69d8bed6382424f1d38239fdb22ce6
Anna Kobylinska <annakobylinska@MacBookAir.local>
2021-08-06 22:02:43 +0200

    first commit

---
+++ pancreas/pancreas.py
@@ -0,0 +1,33 @@
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+def test(a,b)
+    return a+b
+    
+def calc_marker_impact(marker_name, data, q = 10):
+    data['quantile'] = pd.qcut(data[marker_name],q)
+    df2 = data.groupby (['quantile'])['diagnosis'].agg([
+        ('all','count'),
+        ('control', lambda x: sum([1 if x_i ==1 else 0 for x_i in x])),
+        ('benign', lambda x: sum([1 if x_i ==2 else 0 for x_i in x])),
+        ('cancer', lambda x: sum([1 if x_i ==3 else 0 for x_i in x])),
+    ])
+    df2['perc'] = df2['cancer']/df2['all']
+    y_1 = df2['perc'].values
+    x_1 = df2.index.values
+    labels =  [x_i.left for x_i in x_1]
+    data.drop(['quantile'], axis='columns', inplace=True)
+    return y_1, labels
+
+def draw_lineplot(y,labels,col,fig, title):
+    ax = fig.add_subplot(1,1,1)
+    x = range(len(y))
+    ax.set_xticks( list(x))
+    ax.set_xticklabels(labels)
+    ax.plot(x, y, marker='o', markerfacecolor=col ,markersize=12, color=col, linewidth=4)
+    plt.title(title)
+    
+colors = cm.rainbow(np.linspace(0, 2, 10))
---
+++ pancreas/setup.py
@@ -0,0 +1,10 @@
+from glob import glob
+from os.path import basename, splitext
+from setuptools import find_packages, setup
+
+setup(
+    name='pancreas',
+    version='0.1',
+    packages=['pancreas'],
+    url='https://github.com/trombulina/pancreas'
+)
